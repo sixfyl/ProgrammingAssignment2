644ad9d8989d417927ae6c01d1f393b23822691d
Nikolas Cyr <nikcyr@gmail.com>
2015-12-27 16:50:18 -0600

    Initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,18 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Creates a list with items that can be called to set or call the inverse of a matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+inv <- NULL
+set <-function(y) {
+  x <<- y
+  inv <-- NULL
+}
+get <- function() x
+setinv <- function(solve) inv <<- solve
+getinv <- function() inv
+list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
